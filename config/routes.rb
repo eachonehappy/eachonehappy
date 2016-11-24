4c57da839ad88fa465c1b89af9ced5e4c2c403b5
Aditya Singh <work.asingh@gmail.com>
2016-11-24 17:14:45 +0530

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+	root 'pages#home'
+	
+  get 'pages/home'
+
+  get 'pages/about_us'
+
+  get 'pages/contact_us'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
