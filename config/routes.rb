6902867f07499cc0ef366a45495439784105f947
Aditya Singh <work.asingh@gmail.com>
2016-11-30 20:03:46 +0530

    added name field in users

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 	get 'pages/contact_us'
 
-	devise_for :users, controllers: { sessions: 'users/sessions' }
+	devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
 	resources :posts
 	resources :categories
