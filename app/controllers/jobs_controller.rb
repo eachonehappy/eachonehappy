2924cba6197e039d7a79cd8bfdc7e88f8b16d84d
Aditya Singh <work.asingh@gmail.com>
2016-12-01 23:33:27 +0530

    jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,39 @@
 class JobsController < ApplicationController
+	before_action :authenticate_user!
+	def index
+    @jobs = Job.all
+  end
+  
+  def show
+    @job = Job.find(params[:id]) 
+  end
+  
+  def new
+    @job = Job.new
+    @all_campaign = Campaign.all
+  end
+  
+  def create
+  	@job = Job.new(job_params)
+  	@job.campaign_id = params[:campaign][:id]
+  	@job.job_users.build(:user_id => current_user.id)
+    if @job.save
+      flash[:success] = "job created!"
+      redirect_to jobs_path
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    Job.find(params[:id]).destroy
+    flash[:success] = "Job deleted"
+    redirect_to jobs_path
+  end
+  
+  private
+    
+    def job_params
+      params.require(:job).permit(:subject, :description, :campaign)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201113827) do
+ActiveRecord::Schema.define(version: 20161201173332) do
 
   create_table "campaign_users", force: :cascade do |t|
     t.integer  "campaign_id"
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 20161201113827) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "job_users", force: :cascade do |t|
+    t.integer  "job_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "jobs", force: :cascade do |t|
