6ec9e030bf468fae7243a59138c5ce6df76c2acf
Aditya Singh <work.asingh@gmail.com>
2016-12-26 02:30:25 +0530

    added share button gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery_ujs
 //= require cable
 //= require turbolinks
+//= require social-share-button
 //= require_tree .
-//@ sourceMappingURL=jquery.min.map
---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,5 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w(facebook twitter google_plus weibo qq douban google_bookmark
+                          delicious tumblr pinterest email linkedin wechat vkontakte
+                          xing hacker_news)
+end
