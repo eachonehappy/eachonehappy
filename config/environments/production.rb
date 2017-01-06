9b02a35c0cd1097c8ea7d848b4da6aa5b3a9edfb
Aditya Singh <work.asingh@gmail.com>
2017-01-06 22:10:45 +0530

    deploy changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #config.action_cable.url = "http://148.72.244.194/"
+  ActionCable.server.config.disable_request_forgery_protection = true
   config.action_cable.allowed_request_origins = ["https://takestand.in"]                                            
 end
