65c58e4385ca45fa144528a3de55070f106e7bb8
Aditya Singh <work.asingh@gmail.com>
2017-01-09 20:25:37 +0530

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,12 +51,9 @@ set :config_files, %w{config/database.yml config/secrets.yml}
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+  before 'check:linked_files', 'config:push'
+ 
+  before 'deploy:migrate', 'deploy:db:create'
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
