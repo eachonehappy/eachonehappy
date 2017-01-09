912865f9b1caf80b0b9672555e4493d3c9e49f1f
Aditya Singh <work.asingh@gmail.com>
2017-01-09 14:37:51 +0530

    deploy changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.url = "wss://www.takestand.in/cable"
+  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://www.takestand.in','http://www.takestand.in']                                            
 end
