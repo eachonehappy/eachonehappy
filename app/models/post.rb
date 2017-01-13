331627ebfbe45ba547fcd09564a2ce2afe9c40aa
Aditya Singh <work.asingh@gmail.com>
2017-01-13 21:52:34 +0530

    minor fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,5 @@ class Post < ApplicationRecord
 	belongs_to :user
 
     
-  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
+  #tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
 end
